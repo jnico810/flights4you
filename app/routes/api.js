ce0e6f04a6b32eb693a379269709ee6bf221243d
John Nico <jnico810@gmail.com>
2017-01-18 15:22:16 -0500

    Add serverside location lookup

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,20 @@
 // Module for API Routes (serving JSON)
 
 module.exports = function(app) {
-	// var mongoose = require('mongoose'),
-  app.get('/api/airport', function(req, res) {
+  const bodyParser = require('body-parser');
+  const request = require('request');
+  app.get('/api/closestAirport', function(req, res) {
 		// Displaying an already made view
-		res.status(200).send('airline test');
+    console.log("ok");
+    const lat = req.query.lat;
+    const lng = req.query.lng;
+    console.log(lat);
+    console.log(lng);
+    request(`http://iatageo.com/getCode/${lat}/${lng}`, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        console.log('YAHOOO');
+        res.status(200).send(JSON.parse(body));
+      }
+    });
 	});
 };
